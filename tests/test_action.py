2fc47ab0f43d93354062b73603a650243d4f4da9
Hravan <krzysztof.krenc@gmail.com>
2026-03-06 17:24:49 +0100

    Reorganized to not use a class.

--- tests/test_action.py
+++ tests/test_action.py
@@ -1,13 +1,12 @@
 from reinforcement_learning.action import Action
 
 
-class TestAction:
-    def test_perform(self):
-        action = Action(value=1, std=0)
-        assert action.perform() == 1
-    
-    def test_gaussian(self, mocker):
-        mocker.patch('numpy.random.normal', return_value=2)
-        action = Action.gaussian(0, 1)
-        assert action.value == 2
-        assert action.std == 1
+def test_perform():
+    action = Action(value=1, std=0)
+    assert action.perform() == 1
+
+def test_gaussian(mocker):
+    mocker.patch('numpy.random.normal', return_value=2)
+    action = Action.gaussian(0, 1)
+    assert action.value == 2
+    assert action.std == 1
