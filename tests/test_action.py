fc62bd10807ce4110a68e71977e9c90035785c36
Hravan <krzysztof.krenc@gmail.com>
2026-03-06 17:17:31 +0100

    Added a test for Action.gaussian.

--- tests/test_action.py
+++ tests/test_action.py
@@ -5,3 +5,9 @@ class TestAction:
     def test_perform(self):
         action = Action(value=1, std=0)
         assert action.perform() == 1
+    
+    def test_gaussian(self, mocker):
+        mocker.patch('numpy.random.normal', return_value=2)
+        action = Action.gaussian(0, 1)
+        assert action.value == 2
+        assert action.std == 1
