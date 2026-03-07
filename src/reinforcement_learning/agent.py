b663dfaf0ff1cda57b291750e3b67e6db7238ead
Hravan <krzysztof.krenc@gmail.com>
2026-03-07 13:31:02 +0100

    Added EpsilonGreedyAgent.

--- src/reinforcement_learning/agent.py
+++ src/reinforcement_learning/agent.py
@@ -1,13 +1,33 @@
 import random
+import numpy as np
 
 
 class Agent:
     def __init__(self, *actions):
         self.actions = actions
         self.rewards = [[] for _ in self.actions]
+        self.action_history = []
     
     def act(self):
-        action_index = random.randrange(len(self.actions))
+        action_index = self.choice()
+        self.action_history.append(action_index)
         reward = self.actions[action_index].perform()
         self.rewards[action_index].append(reward)
     
+    def choice(self):
+        action_index = random.randrange(len(self.actions))
+        return action_index
+
+
+class EpsilonGreedyAgent(Agent):
+    def __init__(self, *actions, epsilon=0):
+        super().__init__(*actions)
+        self.epsilon = epsilon
+
+    def choice(self):
+        if random.random() > 1 - self.epsilon:
+            action_index = random.randrange(len(self.actions))
+        else:
+            reward_means = [np.mean(action_rewards) for action_rewards in self.rewards]
+            action_index = np.argmax(reward_means)
+        return action_index
--- tests/test_agent.py
+++ tests/test_agent.py
@@ -1,11 +1,35 @@
-from reinforcement_learning.agent import Agent
+from reinforcement_learning.agent import Agent, EpsilonGreedyAgent
 from reinforcement_learning.action import Action
 
 
-def test_deterministic_action():
+def test_deterministic_action_history():
     action = Action(1, 0)
     agent = Agent(action)
     agent.act()
     assert agent.rewards == [[1]]
     agent.act()
     assert agent.rewards == [[1, 1]]
+
+def test_two_deterministic_actions_history(mocker):
+    optimal_action = Action(1, 0)
+    subotpimal_action = Action(-1, 0)
+    agent = Agent(optimal_action, subotpimal_action)
+    mocker.patch('random.randrange', return_value=0)
+    agent.act()
+    mocker.patch('random.randrange', return_value=1)
+    agent.act()
+    
+    assert agent.rewards == [[1], [-1]]
+
+def test_epsilon_greedy_action_choice(mocker):
+    optimal_action = Action(1, 0)
+    subotpimal_action = Action(-1, 0)
+    agent = EpsilonGreedyAgent(optimal_action, subotpimal_action, epsilon=0.1)
+    mocker.patch('random.random', return_value=0.91)
+    mocker.patch('random.randrange', return_value=0)
+    agent.act()
+    mocker.patch('random.randrange', return_value=1)
+    agent.act()
+    mocker.patch('random.random', return_value=0.1)
+    agent.act()
+    assert agent.action_history == [0, 1, 0]
